c2797978b68d8e25809822eb7b0d162513a37b5b
Austin <abingham@roastbeef.(none)>
2009-05-17 09:26:15 +0200

    python comments and multiline strings

--- src/pyloc/python.py
+++ src/pyloc/python.py
@@ -1,19 +1,42 @@
+from ply.lex import TOKEN
+
 class LOCLexer:
-    tokens = ( 'COMMENT',
+    tokens = ( 'COMMENT_LINE',
                #'ESCAPED_SINGLE_QUOTE',
                #'ESCAPED_DOUBLE_QUOTE',
                #'SINGLE_QUOTE',
                #'SINGLE_DOUBLE_QUOTE',
-               #'TRIPLE_QUOTE',
-               #'TRIPLE_DOUBLE_QUOTE', 
+               'MULTILINE_QUOTE',
+               'MULTILINE_DOUBLE_QUOTE', 
                )
 
-    def t_COMMENT(self, t):
-        r'[\t\ ]*\#.*\n'
-        self.line_count += 1
+    space = r'[\t\ ]*'
+    escaped_quote = r'(\\\')*'
+    escaped_double_quote = r'(\\\")*'
+
+    @TOKEN(space + r'\#')
+    def t_COMMENT_LINE(self, t):
         if self.empty_line:
             self.comment_line_count += 1
-        self.empty_line = True
+        self.empty_line = False
+
+    @TOKEN(space + r"'''(.|\n)*?" + escaped_quote + "'''")
+    def t_MULTILINE_QUOTE(self, t):
+        lines = len(t.value.split('\n'))
+        if self.empty_line:
+            self.docstring_line_count += lines
+
+        self.empty_line = False
+        self.lexer.lineno += lines - 1
+
+    @TOKEN(space + r'"""(.|\n)*?' + escaped_double_quote + '"""')
+    def t_MULTILINE_DOUBLE_QUOTE(self, t):
+        lines = len(t.value.split('\n'))
+        if self.empty_line:
+            self.docstring_line_count += lines
+
+        self.empty_line = False
+        self.lexer.lineno += lines - 1
 
     #t_ESCAPED_SINGLE_QUOTE = r"\'"
     #t_ESCAPED_DOUBLE_QUOTE = r'\"'
@@ -22,26 +45,32 @@ class LOCLexer:
     #t_TRIPLE_QUOTE = r"'''"
     #t_TRIPLE_DOUBLE_QUOTE = r'"""'
 
+    @TOKEN(space + r'\n')
     def t_newline(self, t):
-        r'[\t\ ]*\n'
         if self.empty_line:
             self.empty_line_count += 1
-        self.line_count += 1
+        self.lexer.lineno += 1
         self.empty_line = True
 
     def t_error(self, t):
         self.empty_line = False
         t.lexer.skip(1) 
 
+    def line_count(self):
+        return self.lexer.lineno - 1
+
     def build(self, **kwargs):
         import ply.lex
         self.lexer = ply.lex.lex(object=self,**kwargs)
 
-    def __init__(self):
-        self.line_count = 0
+    def __init__(self, **kwargs):
         self.empty_line_count = 0
         self.empty_line = True
+
         self.comment_line_count = 0
+
+        self.quote_type = None
+        self.docstring_line_count = 0
 
 def p_string(t):
     'string : SINGLE_QUOTE text SINGLE_QUOTE | SINGLE_DOUBLE_QUOTE text SINGLE_DOUBLE_QUOTE | TRIPLE_QUOTE text TRIPLE_QUOTE | TRIPLE_DOUBLE_QUOTE text TRIPLE_DOUBLE_QUOTE'
@@ -75,4 +104,22 @@ def loc(filename):
         tok = l.lexer.token()
         if not tok: break
 
-    return l.line_count - l.empty_line_count, l.line_count
+    return l.line_count() - l.empty_line_count, l.line_count()
+
+if __name__ == '__main__':
+    l = LOCLexer()
+    l.build()
+
+    import cStringIO
+    io = cStringIO.StringIO()
+    io.write(open('test_data.py', 'r').read())
+    l.lexer.input(io.getvalue())
+    while True:
+        tok = l.lexer.token()
+        if not tok: break
+        
+    print io.getvalue()
+    print 'total:',    l.line_count()
+    print 'empty:',    l.empty_line_count
+    print 'comment:',  l.comment_line_count
+    print 'docstring:',l.docstring_line_count
--- unittests/python_tests.py
+++ unittests/python_tests.py
@@ -19,7 +19,7 @@ class LexerTests(unittest.TestCase):
     def test_empty_file(self):
         process_input(self.l, '')
             
-        self.assert_(self.l.line_count == 0)
+        self.assert_(self.l.line_count() == 0)
         self.assert_(self.l.empty_line_count == 0)
 
     def test_simple(self):
@@ -29,7 +29,7 @@ class LexerTests(unittest.TestCase):
   """a docstring"""
   x = 1
 ''')
-        self.assert_(self.l.line_count == 3)
+        self.assert_(self.l.line_count() == 3)
         self.assert_(self.l.empty_line_count == 0)
         
     def test_all_empty(self):
@@ -39,8 +39,8 @@ class LexerTests(unittest.TestCase):
 
 
 ''')
-        self.assert_(self.l.line_count == 3)
-        self.assert_(self.l.empty_line_count == self.l.line_count)
+        self.assert_(self.l.line_count() == 3)
+        self.assert_(self.l.empty_line_count == self.l.line_count())
 
     def test_middle_line(self):
         process_input(
@@ -49,7 +49,7 @@ class LexerTests(unittest.TestCase):
 asdf
 
 ''')
-        self.assert_(self.l.line_count == 3)
+        self.assert_(self.l.line_count() == 3)
         self.assert_(self.l.empty_line_count == 2)
 
     def test_basic_comments(self):
@@ -61,9 +61,111 @@ asdf
   # another comment
   y = 3
 ''')
-        self.assert_(self.l.line_count == 5)
+        self.assert_(self.l.line_count() == 5)
         self.assert_(self.l.empty_line_count == 0)
         self.assert_(self.l.comment_line_count == 2)
+
+    def test_multiline_basic(self):
+        process_input(
+            self.l,
+            """def foo():
+  '''this is 
+
+a multi
+line
+comment'''
+
+  '''technically, so is this'''
+""")
+        self.assert_(self.l.line_count() == 8)
+        self.assert_(self.l.empty_line_count == 1)
+        self.assert_(self.l.comment_line_count == 0)
+        self.assert_(self.l.docstring_line_count == 6)
+
+    def test_multiline_hash(self):
+        process_input(
+            self.l,
+            """
+def llama():
+  ''' # this is 
+part # of a 
+# multiline
+comment'''
+
+  '''hopefully this all # works '''
+""")
+        self.assert_(self.l.line_count() == 8)
+        self.assert_(self.l.empty_line_count == 2)
+        self.assert_(self.l.comment_line_count == 0)
+        self.assert_(self.l.docstring_line_count == 5) 
+
+    def test_multiline_and_comment(self):
+        process_input(
+            self.l,
+            """def yak():
+  '''basic docstring # no comment'''
+  # regular comment
+ 
+  x = 1 # another comment
+  y = '''a multiline
+non-docstring''' # this is a comment
+  # as is this '''hehehe'''
+""")
+        self.assert_(self.l.line_count() == 8)
+        self.assert_(self.l.empty_line_count == 1)
+        self.assert_(self.l.comment_line_count == 2)
+        self.assert_(self.l.docstring_line_count == 1)
+
+    def test_multiline_double_basic(self):
+        process_input(
+            self.l,
+            '''def foo():
+  """this is 
+
+a multi
+line
+comment"""
+
+  """technically, so is this"""
+''')
+        self.assert_(self.l.line_count() == 8)
+        self.assert_(self.l.empty_line_count == 1)
+        self.assert_(self.l.comment_line_count == 0)
+        self.assert_(self.l.docstring_line_count == 6)
+
+    def test_multiline_double_hash(self):
+        process_input(
+            self.l,
+            '''
+def llama():
+  """ # this is 
+part # of a 
+# multiline
+comment"""
+
+  """hopefully this all # works """
+''')
+        self.assert_(self.l.line_count() == 8)
+        self.assert_(self.l.empty_line_count == 2)
+        self.assert_(self.l.comment_line_count == 0)
+        self.assert_(self.l.docstring_line_count == 5) 
+
+    def test_multiline_double_and_comment(self):
+        process_input(
+            self.l,
+            '''def yak():
+  """basic docstring # no comment"""
+  # regular comment
+ 
+  x = 1 # another comment
+  y = """a multiline
+non-docstring""" # this is a comment
+  # as is this """hehehe"""
+''')
+        self.assert_(self.l.line_count() == 8)
+        self.assert_(self.l.empty_line_count == 1)
+        self.assert_(self.l.comment_line_count == 2)
+        self.assert_(self.l.docstring_line_count == 1)
 
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(LexerTests)
