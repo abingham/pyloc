cb96259df3f5cf6c965feb032c8353ba043850a6
Austin Bingham <austinb@roxar1313.roxardomain.roxar.com>
2009-05-22 08:40:34 +0200

    updated unittests to reflect new loc() return type

--- unittests/pyloc_tests.py
+++ unittests/pyloc_tests.py
@@ -10,25 +10,17 @@ class Tests(unittest.TestCase):
         pass
 
     def test_python(self):
-        t1_expected = {'comment': 2, 'code': 2, 'total': 5, 'docstring': 2, 'empty': 1}
-        t2_expected = {'comment': 1, 'code': 3, 'total': 5, 'docstring': 1, 'empty': 1}
-        t1 = loc('data/test1.py')
-        t2 = loc('data/test2.py')
-
-        self.assert_(len(t1) == len(t1_expected))
-        self.assert_(len(t2) == len(t2_expected))
-        self.assert_(len(t1) == len(t2))
-
-        for k in t1.keys():
-            self.assert_(t1[k] == t1_expected[k])
-            self.assert_(t2[k] == t2_expected[k])
+        t1_expected = { 'data/test1.py' : ('data/test1.py', 'Python', {'comment': 2, 'code': 2, 'total': 5, 'docstring': 2, 'empty': 1}) }
+        t2_expected = { 'data/test2.py' : ('data/test2.py', 'Python', {'comment': 1, 'code': 3, 'total': 5, 'docstring': 1, 'empty': 1}) }
+        
+        self.assert_(loc('data/test1.py') == t1_expected)
+        self.assert_(loc('data/test2.py') == t2_expected)
 
     def test_python_dir(self):
-        expected = { 'comment' : 3, 'code' : 5, 'total' : 10, 'docstring' : 3, 'empty' : 2 }
-        rslt = loc('data')
-        self.assert_(len(rslt) == len(expected))
-        for k in rslt.keys():
-            self.assert_(rslt[k] == expected[k])
+        expected = { 'data/test1.py' : ('data', 'Python', { 'comment' : 2, 'code' : 2, 'total' : 5, 'docstring' : 2, 'empty' : 1 }),
+                     'data/test2.py' : ('data', 'Python', { 'comment': 1, 'code': 3, 'total': 5, 'docstring': 1, 'empty': 1}) }
+          
+        self.assert_(loc('data') == expected)
 
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(Tests)
