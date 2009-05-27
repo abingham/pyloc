2e9e0a9f63fbb544442bc5976e8494118c0601a7
Austin Bingham <austinb@roxar1313.roxardomain.roxar.com>
2009-05-27 08:21:02 +0200

    added C/C++ to recognized languages
    They are parsed by the standard C++ parser

--- src/pyloc/__init__.py
+++ src/pyloc/__init__.py
@@ -2,6 +2,8 @@ import pyloc.python_source
 import pyloc.cpp_source
 
 lang_map = {
-    '*.py' : ('Python', pyloc.python_source.loc),
-    '*.cpp' : ('C++', pyloc.cpp_source.loc)
+    '*.py'  : ('Python', pyloc.python_source.loc),
+    '*.cpp' : ('C++', pyloc.cpp_source.loc),
+    '*.h'   : ('C/C++ Header', pyloc.cpp_source.loc),
+    '*.hpp'   : ('C++ Header', pyloc.cpp_source.loc),
     }
